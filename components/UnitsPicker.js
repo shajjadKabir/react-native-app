2539d07c5217e329262df258a542f841880d5e20
Anis Islam <manisul1997bd@gmail.com>
2021-06-04 17:14:52 +0600

    unitpicker completed

--- components/UnitsPicker.js
+++ components/UnitsPicker.js
@@ -17,3 +17,22 @@ export default function UnitsPicker({ unitsSystem, setUnitsSystem }) {
         </View>
     )
 }
+
+//style
+const styles = StyleSheet.create({
+    unitsSystem: {
+        position: 'absolute',
+        ...Platform.select({
+            ios: {
+                top: -30,
+            },
+            android: {
+                top: 30,
+            },
+        }),
+
+        left: 20,
+        height: 50,
+        width: 100,
+    },
+})
